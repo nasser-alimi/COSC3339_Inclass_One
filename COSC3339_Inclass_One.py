b8baa203bfbbcef69383d4b75082bb6be1e5642c
nasser-alimi <aalimi@stedwards.edu>
2026-01-16 22:23:45 -0600

    Update COSC3339_Inclass_One.py
    fixed typos in input_checker

--- COSC3339_Inclass_One.py
+++ COSC3339_Inclass_One.py
@@ -71,15 +71,15 @@ def curve_scores(scores):
 # For scenario five fix the typos
 def input_checker(text_value):
 
-    valud_imput = True 
+    valid_input = True 
     
     if text_value is None:
-        valud_imput = False
+        valid_input = False
     
     if text_value == "":
-        valud_imput = False
+        valid_input = False
         
-    return valud_imput
+    return valid_input
     
 def process_user_data(text_value):
     return _validate_imput(text_value)
